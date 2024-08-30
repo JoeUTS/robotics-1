bf6cc760a49d28949deb850339de394a661d2ef0
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2024-08-30 14:11:42 +1000

    Subscribers may not be working correctly in lab4

--- labs/lab4/src/simpledrive.cpp
+++ labs/lab4/src/simpledrive.cpp
@@ -2,7 +2,10 @@
 
 simpledrive::simpledrive() : Node("simpledrive") {
     sub_odo_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/wheel/odometry", 10, std::bind(&simpledrive::odoCallback,this,std::placeholders::_1));
+        "/odom", 10, std::bind(&simpledrive::odoCallback,this,std::placeholders::_1));
+
+    sub_imu_ = this->create_subscription<sensor_msgs::msg::Imu>(
+        "/imu", 10, std::bind(&simpledrive::imuCallback,this,std::placeholders::_1));
 
     pub_vel_ =
         this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
@@ -12,43 +15,87 @@ simpledrive::simpledrive() : Node("simpledrive") {
 
 void simpledrive::odoCallback(const std::shared_ptr<nav_msgs::msg::Odometry> msg) {
     std::unique_lock<std::mutex> lck(mtx_);
+    RCLCPP_INFO_STREAM(this->get_logger(), "Odo updated");
     odo_ = *msg;
 }
 
-void simpledrive::setVelocity(const double xVel, const double yaw) {
+void simpledrive::imuCallback(const std::shared_ptr<sensor_msgs::msg::Imu> msg) {
     std::unique_lock<std::mutex> lck(mtx_);
+    RCLCPP_INFO_STREAM(this->get_logger(), "Imu updated");
+    imu_ = *msg;
+}
+
+nav_msgs::msg::Odometry simpledrive::getOdo(void) {
+    std::unique_lock<std::mutex> lck(mtx_);
+    return odo_;
+}
+
+sensor_msgs::msg::Imu simpledrive::getImu(void) {
+    std::unique_lock<std::mutex> lck(mtx_);
+    return imu_;
+}
+
+void simpledrive::diveCMD(const double x_lin, const double y_lin, const double z_lin, const double roll, const double pitch, const double yaw) {
+    geometry_msgs::msg::Twist cmd;
+
     // linear
-    vel_.linear.x = xVel;
-    vel_.linear.y = 0;
-    vel_.linear.z = 0;
+    cmd.linear.x = x_lin;
+    cmd.linear.y = y_lin;
+    cmd.linear.z = z_lin;
 
     //angular
-    vel_.angular.x = 0;
-    vel_.angular.y = 0;
-    vel_.angular.z = yaw;
+    cmd.angular.x = roll;
+    cmd.angular.y = pitch;
+    cmd.angular.z = yaw;
+    
+    pub_vel_->publish(cmd);
+}
 
-    pub_vel_->publish(vel_);
+void simpledrive::diveCMD(const double x_lin, const double yaw) {
+    geometry_msgs::msg::Twist cmd;
+
+    // linear
+    cmd.linear.x = x_lin;
+    cmd.linear.y = 0;
+    cmd.linear.z = 0;
+
+    //angular
+    cmd.angular.x = 0;
+    cmd.angular.y = 0;
+    cmd.angular.z = yaw;
+
+    pub_vel_->publish(cmd);
 }
 
 int simpledrive::calculateDuration(std::chrono::time_point<std::chrono::system_clock> startTime) {
-    auto endTime = std::chrono::system_clock::now();
+    std::chrono::time_point<std::chrono::system_clock> endTime = std::chrono::system_clock::now();
     int duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
     return duration;
 }
 
-void simpledrive::driveInLine(const double distance, const double velocity) {
+void simpledrive::driveInLine(const double goalDistance, const double goalVelocity) {
     double drivenDistance = 0;
+    double velocity = 0;    // we probs need this as a member variable
 
-    setVelocity(velocity, 0);
-    auto startTime = std::chrono::system_clock::now();
+    std::chrono::time_point<std::chrono::system_clock> startTime = std::chrono::system_clock::now();
 
-    while (drivenDistance < distance) {
+    while (drivenDistance < goalDistance) {
+        //RCLCPP_INFO_STREAM(this->get_logger(), "distance [" << drivenDistance << " / " << goalDistance);
+        //RCLCPP_INFO_STREAM(this->get_logger(), "drivenDistance: " << drivenDistance);
+
+        // calculate driven distance
+        // using IMU cause /odom not updating
+        double acceleration = getImu().linear_acceleration.x;
         int duration = calculateDuration(startTime);
-        drivenDistance += duration * odo_.twist.twist.linear.x;
-        startTime = std::chrono::system_clock::now();
+        velocity += acceleration * duration;
+        drivenDistance += duration * velocity;
 
+        // send command
+        diveCMD(goalVelocity, 0);
+
+        startTime = std::chrono::system_clock::now();
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
-    setVelocity(0, 0);
+    diveCMD(0, 0);
 }
--- labs/lab4/src/simpledrive.h
+++ labs/lab4/src/simpledrive.h
@@ -7,6 +7,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 
 
 class simpledrive: public rclcpp::Node {
@@ -17,17 +18,50 @@ public:
     /**
      * @brief odometry callback
      * 
-     * @param msg nav_msgs::msg::Odometry pointer
+     * @param msg nav_msgs::msg::Odometry
      */
     void odoCallback(const std::shared_ptr<nav_msgs::msg::Odometry> msg);
 
     /**
-     * @brief sets velocitys
+     * @brief imu callback
      * 
-     * @param xVel  Linear velocity [m/s]
-     * @param yaw   Angular velocity [rad/s]
+     * @param msg sensor_msgs::msg::Imu
      */
-    void setVelocity(const double xVel, const double yaw);
+    void imuCallback(const std::shared_ptr<sensor_msgs::msg::Imu> msg);
+
+    /**
+     * @brief Get last odometry message
+     * 
+     * @return nav_msgs::msg::Odometry
+     */
+    nav_msgs::msg::Odometry getOdo(void);
+
+    /**
+     * @brief Get last imu message
+     * 
+     * @return nsensor_msgs::msg::Imu 
+     */
+    sensor_msgs::msg::Imu getImu(void);
+
+    /**
+     * @brief 6DOF publisher
+     * 
+     * @param x_lin [m/s]
+     * @param y_lin [m/s]
+     * @param z_lin [m/s]
+     * @param roll  [rad/s]
+     * @param pitch [rad/s]
+     * @param yaw   [rad/s]
+     */
+    void diveCMD(const double x_lin, const double y_lin, const double z_lin, const double roll, const double pitch, const double yaw);
+
+    /**
+     * @brief 2DOF velocity publisher
+     * 
+     * @param x_lin [m/s]
+     * @param yaw   [rad/s]
+     */
+    void diveCMD(const double x_lin, const double yaw);
 
     /**
      * @brief Sets the starting timer
@@ -44,19 +78,22 @@ public:
     int calculateDuration(std::chrono::time_point<std::chrono::system_clock> startTime);
 
     /**
-     * @brief drive in a square based on dead reckoning
+     * @brief drive for a distance in a straight line
      * 
-     * @param edgeLength [m]
+     * @param goalDistance [m]
+     * @param velocity [m/s]
      */
-    void driveInLine(const double distance, const double velocity);
+    void driveInLine(const double goalDistance, const double goalVelocity);
 
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odo_;  //!< odometry subscriber
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr sub_imu_;  //!< odometry subscriber
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_vel_;     //!< velocity publisher
 
     std::mutex mtx_;    //!< Mutex to allow for future multithreadding. Not Currently required.
 
     nav_msgs::msg::Odometry odo_;   //!< odometry value
-    geometry_msgs::msg::Twist vel_; //!< velocity values
+    sensor_msgs::msg::Imu imu_;   //!< odometry value
+    geometry_msgs::msg::Twist vel_; //!< velocity command values
 };
 
 #endif // SIMPLEDRIVE_H
