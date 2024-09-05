ee212dc931d8531a92b294d1246db6eba0ea3767
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2024-09-05 18:22:30 +1000

    words

--- labs/lab4/src/simpledrive.cpp
+++ labs/lab4/src/simpledrive.cpp
@@ -1,15 +1,23 @@
 #include "simpledrive.h"
 
-simpledrive::simpledrive() : Node("simpledrive") {
+simpledrive::simpledrive() : Node("simpledrive"), GOAL_DISTANCE(1), NOISE_MAX(0.1) {
+    totalDistance_ = 0;
+    vel_.linear.x = 1;  // 1m/s
+
     sub_odo_ = this->create_subscription<nav_msgs::msg::Odometry>(
         "/odom", 10, std::bind(&simpledrive::odoCallback,this,std::placeholders::_1));
 
     pub_cmd_vel_ =
         this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+    pub_noisey_odo_ =
+        this->create_publisher<nav_msgs::msg::Odometry>("/noisey_odo", 10);
     
     timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100),
             std::bind(&simpledrive::timerCallback, this));
+
+    pub_cmd_vel_->publish(vel_);
 }
 
 void simpledrive::odoCallback(const std::shared_ptr<nav_msgs::msg::Odometry> msg) {
@@ -18,49 +26,88 @@ void simpledrive::odoCallback(const std::shared_ptr<nav_msgs::msg::Odometry> msg
     odo_ = *msg;
 }
 
-nav_msgs::msg::Odometry simpledrive::getOdo(void) {
-    std::unique_lock<std::mutex> lck(mtx_);
-    return odo_;
-}
-
 void simpledrive::timerCallback(void) {
-    /*
-    if (not enough ono data) {
+    std::unique_lock<std::mutex> lck(mtx_);
+    // assume not enough odo data yet
+    if (prevOdo_.header.stamp.nanosec == 0 && prevOdo_.header.stamp.sec == 0) {
         return;
     }
-    */
 
+    if (totalDistance_ >= GOAL_DISTANCE) {
+        pub_cmd_vel_->publish(geometry_msgs::msg::Twist());
+        return;
+    }
 
-}
+    // copy member variables
+    nav_msgs::msg::Odometry localOdo = odo_;
+    nav_msgs::msg::Odometry localLastOdo = prevOdo_;
+    geometry_msgs::msg::Twist localvel = vel_;
+    rclcpp::Time localLastTime = lastTime_;
+    lck.unlock();
 
-void simpledrive::diveCMD(const double x_lin, const double y_lin, const double z_lin, const double roll, const double pitch, const double yaw) {
-    geometry_msgs::msg::Twist cmd;
-
-    // linear
-    cmd.linear.x = x_lin;
-    cmd.linear.y = y_lin;
-    cmd.linear.z = z_lin;
-
-    //angular
-    cmd.angular.x = roll;
-    cmd.angular.y = pitch;
-    cmd.angular.z = yaw;
+    // current odometry
+    double localX = localOdo.pose.pose.position.x;
+    double localY = localOdo.pose.pose.position.y;
     
-    pub_cmd_vel_->publish(cmd);
+    // populate orientation
+    double localRoll;
+    double localPitch;
+    double localYaw;
+    tf2::Quaternion quatTF2;
+    tf2::fromMsg(localOdo.pose.pose.orientation, quatTF2);
+    tf2::Matrix3x3(quatTF2).getRPY(localRoll, localPitch, localYaw);
+    
+    // get velocities
+    double velLin = localvel.linear.x;
+    double velYaw = localvel.angular.z;
+
+    // calculate delta time
+    rclcpp::Time currentTime = this->now();
+    double duration = (currentTime - localLastTime).seconds();
+
+    // calculate pose change
+    double distanceTraveled = velLin * duration;
+    double deltaYaw = velYaw * duration;
+
+    // break into individual components
+    double deltaX = distanceTraveled * cos(deltaYaw);
+    double deltaY = distanceTraveled * sin(deltaYaw);
+
+    // update local pose with noise
+    localX += deltaX + getNoise(-NOISE_MAX, NOISE_MAX);
+    localY += deltaY + getNoise(-NOISE_MAX, NOISE_MAX);
+    localYaw += deltaYaw + getNoise(-NOISE_MAX, NOISE_MAX);
+
+    // Yaw correction
+    if (localYaw > M_PI) {
+        localYaw -= 2 * M_PI;
+    } else if (localYaw < -M_PI) {
+        localYaw += 2 * M_PI;
+    }
+
+    // Generate message
+    localOdo.header.stamp = this->now();
+    localOdo.pose.pose.position.x = localX;
+    localOdo.pose.pose.position.y = localY;
+    quatTF2.setRPY(0.0, 0.0, localYaw);
+    localOdo.pose.pose.orientation = tf2::toMsg(quatTF2);
+    localOdo.twist.twist = localvel;
+
+    // update member variables
+    lck.lock();
+    totalDistance_ += distanceTraveled;
+    prevOdo_ = odo_;
+    lastTime_ = this->now();
+    lck.unlock();
+
+    pub_noisey_odo_->publish(localOdo);
 }
 
-void simpledrive::diveCMD(const double x_lin, const double yaw) {
-    geometry_msgs::msg::Twist cmd;
+double simpledrive::getNoise(const double distLower, const double distUpper) {
+    long seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator (seed);
+    std::uniform_real_distribution<double> distribution(distLower,distUpper);
+    double output = distribution(generator);
 
-    // linear
-    cmd.linear.x = x_lin;
-    cmd.linear.y = 0;
-    cmd.linear.z = 0;
-
-    //angular
-    cmd.angular.x = 0;
-    cmd.angular.y = 0;
-    cmd.angular.z = yaw;
-
-    pub_cmd_vel_->publish(cmd);
+    return output;
 }
--- labs/lab4/src/simpledrive.h
+++ labs/lab4/src/simpledrive.h
@@ -3,9 +3,12 @@
 
 #include <chrono>
 #include <thread>
+#include <random>
 
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 
@@ -23,47 +26,31 @@ public:
     void odoCallback(const std::shared_ptr<nav_msgs::msg::Odometry> msg);
 
     /**
-     * @brief Get last odometry message
-     * 
-     * @return nav_msgs::msg::Odometry
-     */
-    nav_msgs::msg::Odometry getOdo(void);
-
-    /**
      * @brief timer function
      * 
      */
     void timerCallback(void);
 
-    /**
-     * @brief 6DOF publisher
-     * 
-     * @param x_lin [m/s]
-     * @param y_lin [m/s]
-     * @param z_lin [m/s]
-     * @param roll  [rad/s]
-     * @param pitch [rad/s]
-     * @param yaw   [rad/s]
-     */
-    void diveCMD(const double x_lin, const double y_lin, const double z_lin, const double roll, const double pitch, const double yaw);
+    
+    double getNoise(const double distLower, const double distUpper);
 
-    /**
-     * @brief 2DOF velocity publisher
-     * 
-     * @param x_lin [m/s]
-     * @param yaw   [rad/s]
-     */
-    void diveCMD(const double x_lin, const double yaw);
-
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odo_;  //!< odometry subscriber
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel_;     //!< velocity publisher
-    rclcpp::TimerBase::SharedPtr timer_; //!< timer
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odo_;      //!< odometry subscriber
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel_;   //!< velocity publisher
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_noisey_odo_;  //!< noisey odometry publisher
+    rclcpp::TimerBase::SharedPtr timer_;                                    //!< timer
 
     std::mutex mtx_;    //!< Mutex to allow for future multithreadding. Not Currently required.
+    std::uniform_real_distribution<double> noiseDist_;  //!< noise
 
-    nav_msgs::msg::Odometry odo_;   //!< odometry value
-    sensor_msgs::msg::Imu imu_;   //!< odometry value
-    geometry_msgs::msg::Twist vel_; //!< velocity command values
+    const double GOAL_DISTANCE; //!< distance to drive
+    const double NOISE_MAX;     //!< max noise value
+
+    nav_msgs::msg::Odometry odo_;       //!< odometry value
+    nav_msgs::msg::Odometry prevOdo_;   //!< previous odometry value
+    geometry_msgs::msg::Twist vel_;     //!< velocity values
+    rclcpp::Time lastTime_; //!< previous timestamp for velocity calulation
+    double totalDistance_;  //!< total distance travelled
+    
 };
 
 #endif // SIMPLEDRIVE_H
